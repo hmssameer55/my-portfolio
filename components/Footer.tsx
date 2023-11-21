57492c99427316015fea51a7ef19595eb4a3a793
hmssameer55 <hmssameer55@gmail.com>
2023-11-21 13:56:48 +0530

    test window

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import SLogo from '../public/images/s.jpg'
+import Script from 'next/script'
+
 type Props = {}
 
 export default function Footer({ }: Props) {
@@ -14,6 +18,18 @@ export default function Footer({ }: Props) {
                     } alt="logo" className="h-8 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
                 </Link>
             </div>
+
+            <Script id="1">
+          {`window.project_id="GJzobNQjUZoQZJih0YGs";
+        window.frame_type="window";
+        `}
+        </Script>
+        <Script
+          id="2"
+          src="https://app.getodin.ai/loader.min.js"
+          project_id="GJzobNQjUZoQZJih0YGs"
+          frame_type="window"
+          defer></Script>
         </footer>
     )
 }
