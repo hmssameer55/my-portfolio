224b605d2691324d34064486c0b25a8cdafe26d4
hmssameer55 <hmssameer55@gmail.com>
2023-11-21 16:20:36 +0530

    check window again

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,27 +10,16 @@ type Props = {}
 
 export default function Footer({ }: Props) {
     return (
-        <footer className="sticky bottom-5 w-14 mx-auto z-20">
-            <div className='flex-center'>
+        <footer className="h-screen  mx-auto z-20 snap-center">
+            {/* <div className='flex-center'>
                 <Link href="#hero">
                     <Image src={
                         SLogo
                     } alt="logo" className="h-8 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
                 </Link>
-            </div>
+            </div> */}
 
-     
-        <Script id="1">
-          {`window.project_id="nQJ1GSps8CRQ1nViNXIo";
-            window.frame_type="widget";
-            `}
-        </Script>
-        <Script
-          id="2"
-          src="https://dev.getodin.ai/loader.min.js"
-          project_id="nQJ1GSps8CRQ1nViNXIo"
-          frame_type="widget"
-          defer></Script>
+
       
                 {/* <Script>
             {`window.embeddedChatbotConfig = {
@@ -44,6 +33,21 @@ export default function Footer({ }: Props) {
             domain="www.chatbase.co"
             defer>
             </Script> */}
+
+                 
+<div id="odin-chatbot">        </div>
+        <Script id="1">
+          {`window.project_id="nQJ1GSps8CRQ1nViNXIo";
+            window.frame_type="window";
+            `}
+        </Script>
+        <Script
+          id="2"
+          src="https://dev.getodin.ai/loader.min.js"
+          project_id="nQJ1GSps8CRQ1nViNXIo"
+          frame_type="window"
+          defer></Script>
+  
         </footer>
     )
 }
