0f5bffeeb4a7c93a0986ee65abee5762346a5a8c
hmssameer55 <hmssameer55@gmail.com>
2023-12-22 18:35:44 +0530

    responsive adjustments

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,52 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import SLogo from '../public/images/s.jpg'
-// import Script from 'next/script'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import SLogo from "../public/images/s.jpg";
+import Script from "next/script";
 
-type Props = {}
+type Props = {};
 
-export default function Footer({ }: Props) {
-    return (
-        <footer className="sticky bottom-5 w-14 mx-auto z-20">
-            <div className='flex-center'>
-                <Link href="#hero">
-                    <Image src={
-                        SLogo
-                    } alt="logo" className="h-8 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" />
-                </Link>
-            </div>
-
-
-      
-                {/* <Script>
-            {`window.embeddedChatbotConfig = {
-            chatbotId: "GPACtlTwnFIHYWhUeQRvn",
-            domain: "www.chatbase.co"
-            }`}
-            </Script>
-            <Script
-            src="https://www.chatbase.co/embed.min.js"
-            chatbotId="GPACtlTwnFIHYWhUeQRvn"
-            domain="www.chatbase.co"
-            defer>
-            </Script> */}
-
-                 
-{/* <div id="odin-chatbot">        </div>
-        <Script id="1">
-          {`window.project_id="nQJ1GSps8CRQ1nViNXIo";
-            window.frame_type="window";
-            `}
-        </Script>
-        <Script
-          id="2"
-          src="https://dev.getodin.ai/loader.min.js"
-          project_id="nQJ1GSps8CRQ1nViNXIo"
-          frame_type="window"
-          defer
-          ></Script> */}
-  
-        </footer>
-    )
+export default function Footer({}: Props) {
+  return (
+    <footer className="sticky bottom-5 w-14 mx-auto z-20">
+      <div className="flex-center">
+        <Link href="#hero">
+          <Image
+            src={SLogo}
+            alt="logo"
+            className="h-8 w-8 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+          />
+        </Link>
+      </div>
+    </footer>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,87 +1,75 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
-import { motion } from 'framer-motion'
-import Link from 'next/link'
-import { Social } from '@typings'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { Social } from "@typings";
 
 type Props = {
-    socials: Social[]
-}
+  socials: Social[];
+};
 
 export default function Header({ socials }: Props) {
-    return (
-        <header className='header'>
-            <motion.div
-                initial={{
-                    x: -500,
-                    opacity: 0,
-                    scale: 0.5
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1
-                }}
-                transition={{
-                    type: 'spring',
-                    stiffness: 440,
-                    damping: 80,
-                }}
-                className="flex-center">
-                {/* <SocialIcon
-                    fgColor='gray'
-                    bgColor='transparent'
-                    url="https://www.instagram.com/hmssameer55/" />
-                <SocialIcon
-                    fgColor='gray'
-                    bgColor='transparent'
-                    url="https://www.linkedin.com/in/hmssameer55/" />
-                <SocialIcon
-                    fgColor='gray'
-                    bgColor='transparent'
-                    url="https://twitter.com/hmssameer55" /> */}
-                {
-                    socials.map((social: Social) => (
-                        <SocialIcon
-                            key={social._id}
-                            fgColor='gray'
-                            bgColor='transparent'
-                            url={social.url}
-                            target='_blank'
-                        />
-                    ))
-                }
-            </motion.div>
-            <motion.div
-                initial={{
-                    x: 500,
-                    opacity: 0,
-                    scale: 0.5
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1
-                }}
-                transition={{
-                    type: 'spring',
-                    stiffness: 200,
-                    damping: 25,
-                }}
-                className='flex-center text-gray-500'>
-                <SocialIcon
-                    network='email'
-                    fgColor='gray'
-                    bgColor='transparent'
-                    className='cursor-pointer'
-                    href='#contact'
-                />
-                <Link href="#contact" className='hidden md:inline-flex'>
-                    Get in touch
-                </Link>
-            </motion.div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 440,
+          damping: 80,
+        }}
+        className="flex-center"
+      >
+        {socials.map((social: Social) => (
+          <SocialIcon
+            key={social._id}
+            fgColor="gray"
+            bgColor="transparent"
+            url={social.url}
+            target="_blank"
+          />
+        ))}
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 200,
+          damping: 25,
+        }}
+        className="flex-center text-gray-500"
+      >
+        <SocialIcon
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+          className="cursor-pointer"
+          href="#contact"
+        />
+        <Link href="#contact" className="hidden md:inline-flex">
+          Get in touch
+        </Link>
+      </motion.div>
+    </header>
+  );
 }
