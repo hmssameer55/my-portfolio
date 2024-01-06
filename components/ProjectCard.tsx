42aa974525e9990c2f1a1b1f3ee85c13ee235ed6
hmssameer55 <hmssameer55@gmail.com>
2024-01-06 13:04:43 +0530

    Update packages, responsive fixes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,53 +1,49 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Project } from '@typings'
-import { urlFor } from '@sanity/helper'
-import ProjectTechCards from './ProjectTechCards'
+import React from "react";
+import { motion } from "framer-motion";
+import { Project } from "@typings";
+import { urlFor } from "@sanity/helper";
+import ProjectTechCards from "./ProjectTechCards";
 
 type Props = {
-    project: Project
-}
+  project: Project;
+};
 
 export default function ProjectCard({ project }: Props) {
+  const { name, summary, image, technologies } = project;
 
-    const { name, summary, image, technologies } = project
+  const handleTruncate = (str: string, n: number) => {
+    return str.length > n ? str.substring(0, n - 1) + "..." : str;
+  };
 
-
-    const handleTruncate = (str: string, n: number) => {
-        return str.length > n ? str.substring(0, n - 1) + '...' : str;
-    }
-
-
-    return (
-        <div className='w-screen flex-shrink-0 snap-center flex-col-center 
-        h-[95vh] space-y-5 p-10 md:p-44'>
-            <motion.img
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 1.5, delay: 0.5 }}
-                viewport={{ once: true }}
-                src={urlFor(image).width(180).height(180).url()}
-                alt={name}
-                className='object-cover rounded-full h-28 w-28 md:h-auto md:w-auto'
-            />
-            <div className='space-y-6 px-0 md:px-10 max-w-6xl'>
-                <h4 className='text-3xl font-semibold text-center capitalize'>
-                    {name}
-                </h4>
-                <div className='flex-center flex-wrap gap-2'>
-                    {
-                        technologies.map((technology, index) => (
-                            <ProjectTechCards key={index} technology={technology} />
-                        ))
-                    }
-                    <p className='text-lg text-center md:text-left pt-2 max-h-[280px] overflow-y-auto pe-2'>
-                        {summary}
-                    </p>
-                </div>
-            </div>
+  return (
+    <div
+      className="w-screen flex-shrink-0 snap-center flex-col-center 
+        h-[95vh] space-y-5 p-10 md:p-44"
+    >
+      <motion.img
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5, delay: 0.5 }}
+        viewport={{ once: true }}
+        src={urlFor(image).width(150).height(150).url()}
+        alt={name}
+        className="object-cover rounded-full h-28 w-28 md:h-auto md:w-auto"
+      />
+      <div className="space-y-6 px-0 md:px-10 max-w-6xl">
+        <h4 className="text-3xl font-semibold text-center capitalize">
+          {name}
+        </h4>
+        <div className="flex-center flex-wrap gap-2">
+          {technologies.map((technology, index) => (
+            <ProjectTechCards key={index} technology={technology} />
+          ))}
+          <p className="text-base text-center md:text-left pt-2 max-h-[280px] overflow-y-auto pe-2">
+            {summary}
+          </p>
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,31 +1,33 @@
-"use client"
+"use client";
 
-import React from 'react'
-import SectionTitle from './SectionTitle'
-import SkillGrid from './SkillGrid'
-import { Skill } from '@typings'
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import SkillGrid from "./SkillGrid";
+import { Skill } from "@typings";
 
 type Props = {
-    skills: Skill[]
-}
+  skills: Skill[];
+};
 
 export default function Skills({ skills }: Props) {
-    return (
-        <section className='snap-start' id="skills">
-            <div className='flex-col-center relative h-screen overflow-hidden text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto'>
-                <SectionTitle text='Skills' />
-                <h3 className='absolute top-32 text-sm text-gray-500 tracking-[3px] uppercase text-center'>
-                    hover over a skill to see my proficiency
-                </h3>
+  return (
+    <section className="snap-start" id="skills">
+      <div className="flex-col-center relative h-screen overflow-hidden text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto">
+        <SectionTitle text="Skills" />
+        <h3 className="absolute top-32 text-sm text-gray-500 tracking-[3px] uppercase text-center">
+          hover over a skill to see my proficiency
+        </h3>
 
-                <div className='grid grid-cols-4 gap-4 mt-28'>
-                    {
-                        skills.map((skill, index) => (
-                            <SkillGrid key={index} skill={skill} directionLeft={index < 8 ? true : false} />
-                        ))
-                    }
-                </div>
-            </div>
-        </section>
-    )
+        <div className="grid grid-cols-4 gap-5 mt-28">
+          {skills.map((skill, index) => (
+            <SkillGrid
+              key={index}
+              skill={skill}
+              directionLeft={index < 8 ? true : false}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
