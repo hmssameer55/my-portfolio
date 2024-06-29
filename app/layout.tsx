df868a60da40a760ad973a4bdf138c1f8f113a73
Sameer Ahmed <85606688+hmssameer55@users.noreply.github.com>
2024-06-29 10:42:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@styles/globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
+import Script from 'next/script';
 
 const inter = Montserrat({ subsets: ['latin-ext'] })
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+                    <Script id="clarity-script" strategy="afterInteractive">
+                {`
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "mzd5swhack");
+                `}
+            </Script>
+        {children}</body>
     </html>
   )
 }
